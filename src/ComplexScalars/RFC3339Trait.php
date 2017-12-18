f5df9c8c01aa03d4ac17f4926ab9e7179186449e
Adam Ainsworth <adam.ainsworth@funeralzone.co.uk>
2017-12-18 16:37:20 +0000

    Add RFC3339 trait

---
+++ src/ComplexScalars/RFC3339Trait.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Funeralzone\ValueObjectExtensions\ComplexScalars;
+
+use Funeralzone\ValueObjects\ValueObject;
+use Carbon\Carbon;
+
+trait RFC3339Trait
+{
+    /**
+     * @var \DateTime
+     */
+    protected $rfc3339;
+
+    /**
+     * RFC3339Trait constructor.
+     * @param \DateTime $rfc3339
+     */
+    public function __construct(\DateTime $rfc3339)
+    {
+        $this->rfc3339 = $rfc3339;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNull(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @param ValueObject $object
+     * @return bool
+     */
+    public function isSame(ValueObject $object): bool
+    {
+        return ($this->toNative() === $object->toNative());
+    }
+
+    /**
+     * @param string $native
+     * @return static
+     */
+    public static function fromNative($native)
+    {
+        if (!is_string($native)) {
+            throw new \InvalidArgumentException('Can only instantiate this object with a string.');
+        }
+
+        $datetime = \DateTime::createFromFormat(\DateTime::RFC3339, $native);
+        if (!$datetime) {
+            throw new \InvalidArgumentException('Can only instantiate this object with a valid RFC3339 string.');
+        }
+
+        return new self($datetime);
+    }
+
+    /**
+     * @return string
+     */
+    public function toNative()
+    {
+        return Carbon::createFromTimestamp($this->rfc3339->getTimestamp(), $this->rfc3339->getTimezone())->toRfc3339String();
+    }
+}
