6c192c00307f61d3a2b43d2901293a480704bb4d
Chris Harrison <chris.harrison@funeralzone.co.uk>
2018-01-24 17:35:19 +0000

    Removed Carbon from tests

--- src/ComplexScalars/RFC3339TraitTest.php
+++ src/ComplexScalars/RFC3339TraitTest.php
@@ -6,21 +6,22 @@ declare(strict_types=1);
 
 namespace Funeralzone\ValueObjectExtensions\ComplexScalars;
 
+use const DATE_RFC3339;
+use DateTimeImmutable;
 use Funeralzone\ValueObjects\ValueObject;
 use PHPUnit\Framework\TestCase;
-use Carbon\Carbon;
 
 final class RFC3339TraitTest extends TestCase
 {
     public function test_is_null_returns_false()
     {
-        $test = new _RFC3339Trait(Carbon::create(1992, 8, 25, 20, 57, 8));
+        $test = new _RFC3339Trait(new DateTimeImmutable);
         $this->assertFalse($test->isNull());
     }
 
     public function test_is_same_returns_true_when_values_match()
     {
-        $rfc3339 = Carbon::create(1992, 8, 25, 20, 57, 8);
+        $rfc3339 = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2010-01-01 00:00:00');
 
         $test1 = new _RFC3339Trait($rfc3339);
         $test2 = new _RFC3339Trait($rfc3339);
@@ -30,8 +31,8 @@ final class RFC3339TraitTest extends TestCase
 
     public function test_is_same_returns_false_when_values_mismatch()
     {
-        $rfc33391 = Carbon::create(1992, 8, 25, 20, 57, 8);
-        $rfc33392 = Carbon::create(2011, 4, 28, 15, 20, 19);
+        $rfc33391 = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2010-01-01 00:00:00');
+        $rfc33392 = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2010-01-01 00:02:00');
 
         $test1 = new _RFC3339Trait($rfc33391);
         $test2 = new _RFC3339Trait($rfc33392);
@@ -41,7 +42,7 @@ final class RFC3339TraitTest extends TestCase
 
     public function test_from_native_instantiates_with_valid_string()
     {
-        $rfc3339String = Carbon::create(1992, 8, 25, 20, 57, 8)->toRfc3339String();
+        $rfc3339String = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2010-01-01 00:00:00')->format(DATE_RFC3339);
 
         $test = _RFC3339Trait::fromNative($rfc3339String);
         $this->assertEquals($rfc3339String, $test->toNative());
@@ -55,10 +56,10 @@ final class RFC3339TraitTest extends TestCase
 
     public function test_to_native_returns_correct_string_representation_of_rfc3339()
     {
-        $rfc3339 = Carbon::create(1992, 8, 25, 20, 57, 8);
+        $rfc3339 = DateTimeImmutable::createFromFormat('Y-m-d H:i:s', '2010-01-01 00:00:00');
 
         $test = new _RFC3339Trait($rfc3339);
-        $this->assertEquals($rfc3339->toRfc3339String(), $test->toNative());
+        $this->assertEquals($rfc3339->format(DATE_RFC3339), $test->toNative());
     }
 }
 
